4cd916341370b954d495cf1bff1ccd14622eebff
ukiuni <ukiuni@gmail.com>
2015-11-15 20:59:15 +0900

    can show appended image on secure content and cookie secured.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 var multer = require('multer');
 var ECT = require('ect');
 var env = process.env.NODE_ENV || "development";
@@ -16,6 +17,7 @@ app.engine('ect', ectRenderer.render);
 app.set('view engine', 'ect');
 app.set('port', process.env.PORT || 3030);
 app.set('views', path.join(__dirname, 'views'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
 	extended : false
 }));
